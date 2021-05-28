deea074b8b4ec7dc5c7d58c79c21f55c1ce97977
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-05-28 10:42:50 -0500

    save changes before installing redux

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,93 +1,108 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Col, Form, FormControl, InputGroup, Row } from 'react-bootstrap'
-import Loader from './Loader/Loader'
-import MovieCard from './MovieCard'
-import NoMoviesFound from './NoMoviesFound'
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Col,
+  Form,
+  FormControl,
+  InputGroup,
+  Row,
+} from "react-bootstrap";
+import Loader from "./Loader/Loader";
+import MovieCard from "./MovieCard";
+import NoMoviesFound from "./NoMoviesFound";
 
 export default function MoviePage() {
-  const [search, setSearch] = useState("")
-  const [movies, setMovies] = useState([])
-  const [loading, setLoading] = useState(false)
-  const API_KEY = process.env.REACT_APP_OMDB_API_KEY
+  const [search, setSearch] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [username, setUsername] = useState("User");
+  const API_KEY = "7bd43d9f";
 
   useEffect(() => {
     const getDefaultMovies = async () => {
-      setLoading(true)
-      const response = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=Dark Knight`, {
-        headers: { Accept: "application/json" },
-      });
+      setLoading(true);
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${API_KEY}&s=Dark Knight`,
+        {
+          headers: { Accept: "application/json" },
+        }
+      );
       const parsedData = await response.json();
       setMovies(parsedData.Search);
-      setLoading(false)
+      setLoading(false);
     };
-    getDefaultMovies()
-  }, [])
-  
+    getDefaultMovies();
+  }, []);
+
   const getMovies = async () => {
-    setLoading(true)
-    const response = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`, {
-      headers: { Accept: "application/json" },
-    });
+    setLoading(true);
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`,
+      {
+        headers: { Accept: "application/json" },
+      }
+    );
     const parsedData = await response.json();
     if (parsedData) {
-      setMovies(parsedData);
+      setMovies(parsedData.Search);
     } else {
-      setMovies([])
+      setMovies([]);
     }
     setLoading(false);
   };
-  
+
   return (
     <div className="main-page-content">
       <h1 className="main-header">Moviflix</h1>
       <h3 className="sub-header">User, try searching for any Movie</h3>
-      <Form className="search-form"
+      <Form
+        className="search-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          getMovies();
+        }}
         onChange={(e) => {
-          e.preventdefault()
-          getMovies()
-        }}>
+          e.preventDefault();
+          getMovies();
+        }}
+      >
         <InputGroup className="mb-3">
           <FormControl
             placeholder="Search Movies"
             aria-label="Search Movies"
             onChange={(e) => setSearch(e.target.value)}
-            value={setSearch}
+            value={search}
             required
-            />
+          />
           <InputGroup.Append>
-            <Button type="" variant="secondary">
+            <Button type="submit" variant="secondary">
               Search
-              </Button>
+            </Button>
           </InputGroup.Append>
         </InputGroup>
       </Form>
-      {
-        !movies ?
-          <NoMoviesFound /> :
-          ( !loading ? 
-            (
-              <Loader />
-            ) : (
-              <Row className="movie-container">
-                {movies.map((movie) => {
-                  return (
-                    <Col
-                      key={movie.imdbID}
-                      xs={12}
-                      sm={6}
-                      md={4}
-                      lg={3}
-                      className="mb-4"
-                    >
-                      <MovieCard movies={movie} />
-                    </Col>
-                  );
-                })}
-              </Row>
-            )
-          )
-      }
+      {!movies ? (
+        <NoMoviesFound />
+      ) : loading ? (
+        <Loader />
+      ) : (
+        <Row className="movie-container">
+          {movies.map((movie) => {
+            return (
+              <Col
+                key={movie.imdbID}
+                xs={12}
+                sm={6}
+                md={4}
+                lg={3}
+                className="mb-4"
+              >
+                <MovieCard movie={movie} />
+              </Col>
+            );
+          })}
+        </Row>
+      )}
     </div>
-  )
+  );
 }
-
